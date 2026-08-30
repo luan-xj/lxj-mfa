3b639fdb555d92241fdf27da9c1d3d05c1d111e9
lxj <lxj@local>
2026-08-31 03:17:21 +0800

    v1.0.14: 在线更新验证目标版

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lxj.mfa"
         minSdk = 26
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.0.13"
+        versionCode = 14
+        versionName = "1.0.14"
     }
 
     buildTypes {
