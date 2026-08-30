f3555fdf871eafbc88b96ced3bf1938a092d9d41
LXJ0535 <luan-xiaojian@163.com>
2026-08-31 01:33:10 +0800

    Update launcher icon to shield design (user provided), bump to v1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lxj.mfa"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.5"
     }
 
     buildTypes {
